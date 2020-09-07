5e3498d1abfed921e7fd457d2cd3daea85ea5104
martinkarrin <martinkarrin@gmail.com>
2020-09-07 20:55:21 +0200

    fine tune for pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="openseries",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     package_data={"OpenSeries": ["*.json"], ".": ["*.json"]},
     install_requires=["jsonschema", "numpy", "pandas<=1.0.4", "plotly", "python-dateutil",
@@ -16,7 +16,7 @@ setup(
     license="BSD License",
     author="karrmagadgeteer2",
     author_email="martin.karrin@captor.se",
-    description="A package for analyzing financial time series",
+    description="Package for simple financial time series analysis.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
@@ -25,7 +25,7 @@ setup(
         "Intended Audience :: Financial and Insurance Industry",
         "Topic :: Office/Business :: Financial :: Investment",
         "Natural Language :: English",
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
