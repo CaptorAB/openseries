b26ca75993aef3572e8d67fb79f00d3ebe8edef7
Martin Karrin <martinkarrin@gmail.com>
2020-10-18 01:25:56 +0200

    new version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="openseries",
-    version="0.1.15",
+    version="0.1.16",
     packages=find_packages(),
     package_data={"openseries": ["*.json"], ".": ["*.json"]},
     install_requires=["jsonschema", "numpy", "pandas", "plotly", "python-dateutil",
