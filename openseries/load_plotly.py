4f706bce0f27760f96997e2c2e99fe1468823797
martinkarrin <martinkarrin@gmail.com>
2020-10-30 11:55:37 +0100

    fixed file path in load_plotly.py

--- openseries/load_plotly.py
+++ openseries/load_plotly.py
@@ -5,8 +5,8 @@ import os
 def load_plotly_dict() -> (dict, dict):
 
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    layoutfile = os.path.join(os.path.abspath(project_root), 'OpenSeries', 'plotly_layouts.json')
-    logofile = os.path.join(os.path.abspath(project_root), 'OpenSeries', 'plotly_captor_logo.json')
+    layoutfile = os.path.join(os.path.abspath(project_root), 'openseries', 'plotly_layouts.json')
+    logofile = os.path.join(os.path.abspath(project_root), 'openseries', 'plotly_captor_logo.json')
     with open(layoutfile, 'r', encoding='utf-8') as f:
         fig = json.load(f)
     with open(logofile, 'r', encoding='utf-8') as ff:
