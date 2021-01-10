7d79d889f4f65eafb05fcaad9209028a296a1050
martinkarrin <martinkarrin@gmail.com>
2021-01-10 13:30:23 +0100

    incl coverage folder & downgraded coverage version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="openseries",
-    version="0.1.24",
+    version="0.1.25",
     packages=find_packages(),
     package_data={"openseries": ["*.json"], ".": ["*.json"]},
     install_requires=[
