7f246d42df929aea5d2806a79142c6a8f7caebed
martinkarrin <martinkarrin@gmail.com>
2021-12-27 11:54:57 +0100

    reverted requirements.txt

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="openseries",
-    version="0.2.8",
+    version="0.2.9",
     packages=find_packages(),
     package_data={"openseries": ["*.json"], ".": ["*.json", "coverage.svg"]},
     install_requires=[
