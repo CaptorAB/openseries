9ac814bac9ba2e8949cf9f8a8e627ae01887a5f3
martinkarrin <martinkarrin@gmail.com>
2022-08-08 19:11:26 +0200

    trying editable install

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
 
-
-with open("README.md", "r") as read_me:
-    long_description = read_me.read()
-
-setup(long_description=long_description, long_description_content_type="text/markdown")
+setup()
