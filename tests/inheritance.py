4b3bcadfe6d3a5cf1c3149ea61f9ab12a7316451
Martin Karrin <martin.karrin@captor.se>
2023-09-27 09:23:47 +0200

    investigating how to remove multiple inheritance WIP

---
+++ tests/inheritance.py
@@ -0,0 +1,177 @@
+"""Investigating how to remove multiple inheritance structure."""
+from __future__ import annotations
+
+from typing import TypeVar
+
+from pydantic import BaseModel
+
+TypeTopSingle = TypeVar("TypeTopSingle", bound="TopSingle")
+TypeLeftSingle = TypeVar("TypeLeftSingle", bound="LeftSingle")
+TypeRightSingle = TypeVar("TypeRightSingle", bound="RightSingle")
+TypeTopMulti = TypeVar("TypeTopMulti", bound="TopMulti")
+TypeLeftMulti = TypeVar("TypeLeftMulti", bound="LeftMulti")
+TypeRightMulti = TypeVar("TypeRightMulti", bound="RightMulti")
+
+
+class TopSingle(BaseModel):  # type: ignore[misc]
+
+    """
+    Declare TopSingle.
+
+    Parameters
+    ----------
+    a: int
+        a attribute
+
+    Returns
+    -------
+    TopSingle
+        Object of the class TopSingle
+    """
+
+    a: int
+
+
+class LeftSingle(TopSingle):
+
+    """
+    Declare LeftSingle.
+
+    Parameters
+    ----------
+    a: int
+        a attribute
+    b: int
+        b attribute
+
+    Returns
+    -------
+    LeftSingle
+        Object of the class LeftSingle
+    """
+
+    a: int
+    b: int
+
+
+class RightSingle(TopSingle):
+
+    """
+    Declare RightSingle.
+
+    Parameters
+    ----------
+    a: int
+        a attribute
+
+    Returns
+    -------
+    RightSingle
+        Object of the class RightSingle
+    """
+
+    a: int
+    c: tuple[int, int]
+
+    def __init__(self: RightSingle, a: int) -> None:
+        """
+        Declare RightSingle.
+
+        Parameters
+        ----------
+        a: int
+            a attribute
+
+        Returns
+        -------
+        RightSingle
+            Object of the class RightSingle
+        """
+        super().__init__(a=a)
+
+        self.a = a
+        self.c = (1, 2)
+
+
+class TopMulti:
+
+    """
+    Declare TopMulti.
+
+    Parameters
+    ----------
+    a: int
+        a attribute
+
+    Returns
+    -------
+    TopMulti
+        Object of the class TopMulti
+    """
+
+    a: int
+
+
+class LeftMulti(BaseModel, TopMulti):  # type: ignore[misc]
+
+    """
+    Declare LeftMulti.
+
+    Parameters
+    ----------
+    a: int
+        a attribute
+    b: int
+        b attribute
+
+    Returns
+    -------
+    LeftMulti
+        Object of the class LeftMulti
+    """
+
+    a: int
+    b: int
+
+
+class RightMulti(BaseModel, TopSingle):  # type: ignore[misc]
+
+    """
+    Declare TopMulti.
+
+    Parameters
+    ----------
+    a: int
+        a attribute
+
+    Returns
+    -------
+    TopMulti
+        Object of the class TopMulti
+    """
+
+    a: int
+    c: tuple[int, int]
+
+    def __init__(self: RightMulti, a: int) -> None:
+        """
+        Declare TopMulti.
+
+        Parameters
+        ----------
+        a: int
+            a attribute
+
+        Returns
+        -------
+        TopMulti
+            Object of the class TopMulti
+        """
+        super().__init__()
+
+        self.a = a
+        self.c = (1, 2)
+
+
+if __name__ == "__main__":
+    pass
