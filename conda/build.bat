800987bff729897b31b48d3332ae302e58d95062
Martin Karrin <martin.karrin@captor.se>
2023-11-19 16:50:14 +0100

    even more on conda

--- conda/build.bat
+++ conda/build.bat
@@ -1,3 +1,1 @@
 %PYTHON% -m pip install --no-deps --ignore-installed .
-%PYTHON% -m coverage run -m pytest --verbose --capture=no
-%PYTHON% -m coverage report -m
--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 $PYTHON -m pip install --no-deps --ignore-installed .
-$PYTHON -m coverage run -m pytest --verbose --capture=no
-$PYTHON -m coverage report -m
