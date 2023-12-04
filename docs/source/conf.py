70149f1eae944d5e045f9ad8afa13c5b2ae9f24e
Martin Karrin <martin.karrin@captor.se>
2023-12-04 21:03:20 +0100

    trialling docs

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# mypy: disable-error-code="var-annotated"
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "openseries"
+copyright = "2023, Martin Karrin"
+author = "Martin Karrin"
+release = "1.4.9"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
