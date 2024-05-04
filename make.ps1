f0baca6bc2e56583b31c1dd91092a5ecb5faf439
Martin Karrin <martin.karrin@captor.se>
2024-05-04 13:29:33 +0200

    rewrite in make.ps1 to properly set PYTHONPATH

--- make.ps1
+++ make.ps1
@@ -2,15 +2,48 @@ param (
     [string]$task = "active"
 )
 
-if ($task -eq "active") {
-    $env:PYTHONPATH = "$env:PYTHONPATH;$pwd"
+if ($task -eq "active")
+{
+    if ($null -ne $env:PYTHONPATH)
+    {
+        if (-not $env:PYTHONPATH -match [regex]::Escape($PWD))
+        {
+            $env:PYTHONPATH += ";$PWD"
+            Write-Output "PYTHONPATH changed. It is '$( $env:PYTHONPATH )'"
+        }
+        else
+        {
+            Write-Output "PYTHONPATH not changed. It is '$( $env:PYTHONPATH )'"
+        }
+    }
+    else
+    {
+        $env:PYTHONPATH = $PWD
+        Write-Output "PYTHONPATH set to: $( $env:PYTHONPATH )"
+    }
     .\venv\Scripts\activate
 }
-elseif ($task -eq "make") {
-    # remove old environment if present and create new environment
+elseif ($task -eq "make")
+{
     Remove-Item -Path ".\venv" -Recurse -Force -ErrorAction SilentlyContinue
     python -m venv ./venv
-    $env:PYTHONPATH = "$env:PYTHONPATH;$pwd"
+    if ($null -ne $env:PYTHONPATH)
+    {
+        if (-not $env:PYTHONPATH -match [regex]::Escape($PWD))
+        {
+            $env:PYTHONPATH += ";$PWD"
+            Write-Output "PYTHONPATH changed. It is '$( $env:PYTHONPATH )'"
+        }
+        else
+        {
+            Write-Output "PYTHONPATH not changed. It is '$( $env:PYTHONPATH )'"
+        }
+    }
+    else
+    {
+        $env:PYTHONPATH = $PWD
+        Write-Output "PYTHONPATH set to: $( $env:PYTHONPATH )"
+    }
     .\venv\Scripts\activate
     python.exe -m pip install --upgrade pip
     pip install poetry==1.8.2
@@ -18,25 +51,25 @@ elseif ($task -eq "make") {
     poetry install --with dev
     poetry run pre-commit install
 }
-elseif ($task -eq "test") {
-    # run tests and report coverage
+elseif ($task -eq "test")
+{
     poetry run coverage run -m pytest --verbose --capture=no
     poetry run coverage report -m
     poetry run coverage-badge -o coverage.svg -f
 }
-elseif ($task -eq "lint") {
-    # run lint and typing checks
+elseif ($task -eq "lint")
+{
     poetry run ruff check . --fix --exit-non-zero-on-fix
     poetry run mypy .
 }
-elseif ($task -eq "clean") {
-    # remove virtual environment and lock file to start over
+elseif ($task -eq "clean")
+{
     pre-commit uninstall
     deactivate
     Remove-Item -Path ".\venv" -Recurse -Force -ErrorAction SilentlyContinue
     Remove-Item -Path 'poetry.lock' -Force -ErrorAction SilentlyContinue
 }
-else {
-    # invalid task argument
+else
+{
     Write-Host "Only active, make, test, clean or lint are allowed as tasks"
 }
