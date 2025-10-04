855b14028296033d6a03c02b2cf22082f22495bd
Martin Karrin <martin.karrin@captor.se>
2025-10-04 18:40:56 +0200

    removed reference to notebook use in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,6 @@ extensions = [
     "sphinx.ext.githubpages",
     "myst_parser",
     "sphinx_rtd_theme",
-    "nbsphinx",
 ]
 
 # Napoleon settings for Google-style docstrings
@@ -143,9 +142,3 @@ myst_enable_extensions = [
     "substitution",
     "tasklist",
 ]
-
-# nbsphinx configuration
-nbsphinx_execute = "never"  # Don't execute notebooks during build
-nbsphinx_allow_errors = True
-nbsphinx_timeout = 60
-nbsphinx_requirejs_path = ""
